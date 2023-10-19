8254e85367f74ff963b7de1eb530567e1a0d6bc9
Nader Ahmed <naderahmed2024@u.northwestern.edu>
2023-10-18 23:07:49 -0500

    online

--- A_star.py
+++ A_star.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-#test
+
 def read_dat_file(file_path):
     data = []
     with open(file_path, 'r') as file:
@@ -28,7 +28,8 @@ class Node:
 
 
 class A_star: 
-    def __init__(self,x_range, y_range, cell_size): 
+    def __init__(self, m, x_range, y_range, cell_size): 
+        self.m = m 
         self.x_range = x_range 
         self.y_range = y_range
         self.cell_size = cell_size 
@@ -37,26 +38,18 @@ class A_star:
     def generate_grid(self): 
         num_rows = int((self.y_range[1] - self.y_range[0]) / self.cell_size)
         num_cols = int((self.x_range[1] - self.x_range[0]) / self.cell_size)
-        self.grid = np.zeros((num_rows,num_cols)) #grid values are 0(their cost)
+        self.grid = np.zeros((num_rows,num_cols))
         self.populate_landmarks() 
     
     def populate_landmarks(self): 
         landmark_data = read_dat_file('ds1/ds1_Landmark_Groundtruth.dat')
-        landmarks_x, landmarks_y = landmark_data[:,1], landmark_data[:,2] #obtain world positions for landmarks, need to discretize to grid locations
+        landmarks_x, landmarks_y = landmark_data[:,1], landmark_data[:,2]
 
         for l_x, l_y in zip(landmarks_x, landmarks_y): 
             l_idx, l_idy = self.position_to_index(l_x,l_y)
-            self.grid[l_idy,l_idx] = 1000 #[row,col] grid values are 1000 (their cost)
-            
-
-    #TODO: ADD ERROR CHECKING FOR VALID START,GOAL
-    
-
-    #frontier: nodes that have been visited but not expanded (their successors havent been explored) 
-    #visited: nodes that have been visited and expanded
+            self.grid[l_idy,l_idx] = 1 #[row,col]
 
     def plan_path(self, start, goal): 
-        #plan path from start to goal
         start_node = Node(self.position_to_index(*start))
         goal_node = Node(self.position_to_index(*goal))
 
@@ -64,12 +57,12 @@ class A_star:
         visited = [] 
         
         while frontier: 
-            cur_node = min(frontier, key=lambda x:x.f) #select node from neighbors with lowest total cost, f
+            cur_node = min(frontier, key=lambda x:x.f)
 
             visited.append(cur_node) 
             frontier.remove(cur_node) 
 
-            if cur_node == goal_node: #if found goal node, find the path and return 
+            if cur_node == goal_node: 
                 path = [] 
                 while cur_node: 
                     path.append(cur_node.position) 
@@ -78,63 +71,87 @@ class A_star:
             
             neighbors = self.get_neighbors(cur_node) 
 
-            for neigh in neighbors:  #evaluate the cost for all the neighbors
-                print(neighbors)
-                neigh.g = cur_node.g  + self.grid[neigh.position[1],neigh.position[0]]
+            for neigh,cost in neighbors: 
+                neigh.g = cur_node.g + cost 
                 neigh.h = self.manhattan_heuristic(neigh.position,goal_node.position)
                 neigh.f = neigh.g + neigh.h 
 
-                if neigh not in visited and neigh not in frontier: #if this is a new node, add it to list to be expanded
+                if neigh not in visited and neigh not in frontier: 
                     neigh.parent = cur_node 
                     frontier.append(neigh) 
 
-                #check if another node in the frontier has the same position as this neighbor
-                elif neigh in frontier: #this node was already visited, but not expanded. we want the shorter cost path to it.
-                    prev_found_neigh = frontier[frontier.index(neigh)]
-                
-                    if neigh.g < prev_found_neigh.g: #comparing cost of reaching this spot from two diff paths
+                elif neigh in frontier:
+                    prev_found_neigh = frontier[frontier.index(neigh)] 
+                    if neigh.g < prev_found_neigh.g:
                         prev_found_neigh.g = neigh.g
                         prev_found_neigh.f = prev_found_neigh.g + prev_found_neigh.h
                         prev_found_neigh.parent = cur_node
 
         return None
 
-    #manhattan distance 
+    def online_plan_path(self, start, goal):
+        current_position = self.position_to_index(*start)
+        path = [current_position]
+        visited = {current_position}  # Using a set for O(1) lookup time
+
+        while current_position != self.position_to_index(*goal):
+            neighbors = self.get_neighbors(Node(current_position))
+            
+            # If no available steps, we're stuck
+            if not neighbors:
+                print("No available steps from position:", current_position)
+                return path
+
+            # Calculate cost-to-come for each neighbor
+            neighbor_costs = {n[0].position: n[1] + self.manhattan_heuristic(n[0].position, self.position_to_index(*goal)) for n in neighbors if n[0].position not in visited}
+            
+            # Sort neighbors by cost-to-come
+            sorted_neighbors = sorted(neighbor_costs, key=neighbor_costs.get)
+            
+            # If we have exhausted all the neighbors, we're stuck
+            if not sorted_neighbors:
+                print("Stuck at position:", current_position)
+                return path
+            
+            # Pick the lowest-cost neighbor
+            next_position = sorted_neighbors[0]
+
+            # We should ensure it's not a wall before proceeding
+            if neighbor_costs[next_position] < 1000:  # not a wall
+                path.append(next_position)
+                current_position = next_position
+                visited.add(current_position)
+                
+        return path
+
+
     def manhattan_heuristic(self,start,goal): 
         return np.abs(goal[0]-start[0]) + np.abs(goal[1]-start[1])
 
-
-    #softly declare all the landmark positions for the grid
-    #the robot moves as if there is no landmarks
-    #if one of the robot's generated neighbors is a landmark position, 
-        # turn it from gray->black on the grid
-        # "observe" the landmark by setting its cost to 1000 and proceed
-    
     def get_neighbors(self,node): 
         neighbors = []
         pos_x, pos_y = node.position 
         for dx in [-1,0,1]: 
             for dy in [-1,0,1]: 
-                neigh_x, neigh_y = pos_x+dx, pos_y+dy 
-                if self.within_grid(neigh_x,neigh_y) and (dx,dy) != (0,0): 
-                    neighbors.append(Node(position=(neigh_x,neigh_y))) #returns neighbor and movement cost to that node 
-        return neighbors 
+                new_x, new_y = pos_x+dx, pos_y+dy 
 
+                if self.within_grid(new_x,new_y) and (dx,dy) != (0,0): 
+                    if self.grid[new_y,new_x] == 1:
+                        cost = 1000
+                    else: 
+                        cost = 1
+                    neighbors.append((Node(position=(new_x,new_y)),cost)) 
+        return neighbors 
 
     def within_grid(self,x,y): 
         return 0<=x<self.grid.shape[1] and 0 <=y < self.grid.shape[0]
 
-
-    #converts between continuous position to grid index
     def position_to_index(self,pos_x,pos_y): 
-        #1) subtract position from minimum range (to find relative distance from beginning) 
-        #2) divide by cell size 
         idx_x, idx_y = int((pos_x - self.x_range[0]) / self.cell_size) , int((pos_y - self.y_range[0]) / self.cell_size)
         return idx_x, idx_y
 
     def display_grid(self): 
-        # print(self.grid) 
-        pass
+        print(self.grid) 
     
     def visualize_results(self, path):
         plt.figure(figsize=(5,10))
@@ -146,8 +163,7 @@ class A_star:
         plt.imshow(self.grid, cmap='gray_r')
 
         if path:
-            # print("AJSDA",path)
-            path_x, path_y = zip(*path) #unpacks path and 
+            path_x, path_y = zip(*path) 
             plt.plot(path_x, path_y, 'ro-', markersize=5)
 
         plt.title("A* Pathfinding Results")
@@ -155,18 +171,16 @@ class A_star:
 
 
 def main(): 
-    # def __init__(self, m, x_range, y_range, cell_size): 
-
-    test = A_star(x_range=(-2,5),y_range=(-6,6),cell_size=1) 
+    test = A_star(1,(-2,5),(-6,6),1) 
     test.display_grid()
-    path = test.plan_path(start=(0.5,-1.5),goal=(.5,1.5))   
+    path = test.online_plan_path(start=(0.5,-1.5),goal=(.5,1.5))
     test.visualize_results(path)
-    path = test.plan_path(start=(4.5,3.5),goal=(4.5,-1.5))   
+    path = test.online_plan_path(start=(4.5,3.5),goal=(4.5,-1.5))
     test.visualize_results(path)
-    path = test.plan_path(start=(-0.5,5.5),goal=(1.5,3.5))   
+    path = test.online_plan_path(start=(-0.5,5.5),goal=(1.5,3.5))
     test.visualize_results(path)
 
-    # print(path)
+    print(path)
 
 
 if __name__=='__main__': 
